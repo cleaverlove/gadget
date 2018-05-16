8b3eabe6f6b23bbba1d3f44e0ae5c757ccc8d7ea
cleaverlove <a754038686@qq.com>
2018-05-16 11:36:49 +0800

    http静态资源服务

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # gadget
 nodejs练手小项目
+
+## 控制台输入-中英翻译
+  dictionary文件夹
+  
+## cnode社区发布评论/爬取学校网站数据
+  web_crawler文件夹
+  
+## http做的静态资源服务
+  httpServer文件夹
---
+++ httpServer/index.js
@@ -0,0 +1,40 @@
+
+var http = require('http'),
+	fs =  require('fs');
+
+var serve = http.Server();
+//必须打开文件，读取其中的内容，然后将这些内容发送给浏览器
+function serveStaticFile(res, path, contentType, responseCode) {
+	if (!responseCode) responseCode = 200;
+	fs.readFile(__dirname + path, function(err, data) {
+		if (err) {
+			res.writeHead(500, {'Content-Type': 'text/plain'});
+			res.end('Internal Error');
+		} else {
+			res.writeHead(responseCode, {'Content-Type': contentType});
+			res.end(data);
+		}
+	})
+}
+
+serve.on('request', function(req, res) { 
+	//规范化url, 去掉查询字符串，可选的反斜杆，并把它变成小写
+	var path = req.url.replace(/\/?(?:\?.*)?$/,'').toLowerCase();
+	console.log(path);
+	switch(path) {
+		case '':
+			serveStaticFile(res, '/public/home.html', 'text/html');
+			break;
+		case '/about':
+			serveStaticFile(res, '/public/about.html', 'text/html');
+			break;
+		case '/img/logo.jpg':
+			serveStaticFile(res, '/public/img/logo.jpg','image/jpeg');
+		default:
+			serveStaticFile(res, '/public/nofound.html','text/html',404);
+			break;
+	}
+}).listen(3000,'127.0.0.1', function() {
+	console.log('监听成功');
+});
+
